15ff824d8d4865088a562068be9f19c2dcba84ca
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-03-11 23:16:21 +0530

    Initial setup

---
+++ src/main/java/com/ipldashboard/ipldashboard/IplDashboardSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ipldashboard.ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IplDashboardSpringApplication.class, args);
+    }
+
+}
